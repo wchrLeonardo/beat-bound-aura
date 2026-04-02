556b5705c69af57c1c8e6374132aaa9b72eb1b4e
DSM6 <fatec-dsm6@academico.intra>
2026-04-01 22:00:09 -0300

    [FEAT]:Adicionando Login 1/2

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+export default function Login() {
+  return (
+    <main className="min-h-screen flex items-center justify-center relative overflow-hidden">
+      
+      {/* 1. O Fundo Neon (Efeito de Aura) */}
+      <div className="absolute w-[500px] h-[500px] bg-cyan-600/20 rounded-full blur-[120px] -z-10"></div>
+
+      {/* 2. O Card onde ficará o formulário */}
+      <div className="text-white text-2xl font-bold">
+        
+        Área do Card de Login
+      </div>
+
+    </main>
+  );
+}
